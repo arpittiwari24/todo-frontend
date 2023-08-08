bbd8077c7d57397e9ce3899d4e98a057269b6fb0
arpittiwari24 <100207940+arpittiwari24@users.noreply.github.com>
2023-08-08 17:42:04 +0530

    committ

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ShowTask from "./components/ShowTask.jsx";
 import  CreateTask  from "./components/CreateTask.jsx";
-import UpdateTask from "./components/UpdateTask.jsx";
 
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
                 <Routes>
                 <Route path="/" element={<ShowTask />} />
                 <Route path="/create-task" element={<CreateTask />} />
-                <Route path="/update-task" element={<UpdateTask />} />
                 </Routes>
             </BrowserRouter>
     );
