ba47ed729fc7add8e497b40f7d29a3057620d5eb
arpittiwari24 <atofficial2410@gmail.com>
2024-06-06 16:28:10 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa/dist/index.cjs';
+
+const manifestForPlugin = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Todo App",
+    short_name:"todo-app",
+    description:"I am a simple todo app",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+  ],
+  theme_color:'#181818',
+  background_color:'#e0cc3b',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
