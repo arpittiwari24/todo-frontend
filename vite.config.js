6a32793c6be38bc02a5f4c0fddae5742fe97dbe8
arpittiwari24 <atofficial2410@gmail.com>
2024-06-06 16:39:52 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,29 @@ const manifestForPlugin = {
     short_name:"todo-app",
     description:"I am a simple todo app",
     icons:[{
-      src: '/android-chrome-192x192.png',
+      src: '/public/android-chrome-192x192.png',
       sizes:'192x192',
       type:'image/png',
       purpose:'favicon'
     },
+    {
+      src:'/public/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/public/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/public/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
   ],
   theme_color:'#181818',
   background_color:'#e0cc3b',
